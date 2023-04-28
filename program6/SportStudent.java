5c035b5fd659362a5403d7ec82e09b16bfebfae9
anjana-pa <116252306+anjana-pa@users.noreply.github.com>
2023-04-28 14:41:47 +0530

    Add files via upload

---
+++ program6/SportStudent.java
@@ -0,0 +1,33 @@
+import java.util.*;
+interface Student
+{
+int score=10;
+void displayScore();
+}
+interface Sports
+{
+int score=25;
+void displaySportsScore();
+}
+class Result implements Student,Sports
+{
+public void displayScore()
+
+{
+System.out.println("Academic score:"+Student.score);
+}
+public void displaySportsScore()
+{
+System.out.println("Sports score:"+Sports.score);
+}
+}
+class SportStudent
+{
+public static void main(String[] args)
+{
+Result r=new Result();
+r.displayScore();
+r.displaySportsScore();
+} 
+}
+
